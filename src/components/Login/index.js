65fbf820020296ad6d49bb7b8eae6210aeaa8f81
Nakkatarun <tarunvamsi666@gmail.com>
2025-02-21 16:10:51 +0530

    demo

---
+++ src/components/Login/index.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { SignInPage } from '@toolpad/core/SignInPage';
+import { useTheme } from '@mui/material/styles';
+
+// preview-start
+const providers = [{ id: '1', name: {email : "tarunvamsi666@gmail.com", password : "9492804969"} }];
+// preview-end
+
+const signIn = async (provider, formData) => {
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      alert(
+        `Signing in with "${provider.name}" and credentials: ${formData.get('email')}, ${formData.get('password')}`,
+      );
+      resolve();
+    }, 300);
+  });
+  return promise;
+
+};
+
+export default function CredentialsSignInPage() {
+  const theme = useTheme();
+  return (
+    // preview-start
+    <AppProvider theme={theme}>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        slotProps={{ emailField: { autoFocus: false } }}
+      />
+    </AppProvider>
+    // preview-end
+  );
+}
